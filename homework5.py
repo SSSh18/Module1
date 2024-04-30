2bc4a25ed388170394edb0d28d6a80f9de5763ea
SSSh18 <168445136+SSSh18@users.noreply.github.com>
2024-04-30 13:48:09 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,15 @@
+my_list = ['strawberry', 'grape', 'peach', 'pineapple']
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:5])
+my_list[2] = 'melon'
+print(my_list)
+
+my_dict = {'bridge': 'мост', 'railway': 'железная дорога', 'train': 'поезд'}
+print(my_dict)
+print(my_dict['railway'])
+my_dict['underground'] = 'метро' # добавление новой пары в словарь
+print(my_dict)
+my_dict['train'] = 'трамвай' # изменение значения для заданного ключа
+print(my_dict)
