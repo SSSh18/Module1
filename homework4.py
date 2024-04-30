aac7f6fa0dea8cc5de240f87180fb6bafedf22d3
SSSh18 <168445136+SSSh18@users.noreply.github.com>
2024-04-30 12:05:23 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,7 @@
+immutable_var = 1.2, 56, 'city', False
+print(immutable_var)
+#immutable_var[2] = 'town' #кортеж не поддерживает обращение по элементам
+mutable_list = [34, 85.2, 'seal', True]
+mutable_list [0] = 'watermelon'
+mutable_list [2] = 'blueberry'
+print(mutable_list)
